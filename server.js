9fb59b06fb84d59c01e618d10315490e1bf5dfe5
Steven Krenn <stevekrenn@gmail.com>
2023-01-15 15:37:17 -0600

    first commit, server.js added

---
+++ server.js
@@ -0,0 +1,13 @@
+require('./config/connection')
+const express = require('express');
+const routes = require('./routes')
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(routes)
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
