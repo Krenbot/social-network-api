7c4e606fcbf22c644d804116f4506d74da018784
Steve Krenn <stevekrenn@gmail.com>
2023-01-18 17:44:19 -0600

    added thought controller

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,2 +1,40 @@
 //addReaction - thoughts find one and update ONLY UPDATE
 //removeReaction - ONLY UPDATE
+const { Thought } = require('../models')
+
+module.exports = {
+    create: async function (req, res) {
+        try {
+            const result = await Thought.create(req.body)
+            res.json(result)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    find: async function (req, res) {
+        try {
+            const result = await Thought
+                .find()
+                .populate('user')
+            res.json(result)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    update: async function (req, res) {
+        try {
+            const result = await Thought.findByIdAndUpdate(req.params.id, req.body, { new: true })
+            res.json(result)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    delete: async function (req, res) {
+        try {
+            const result = await Thought.findByIdAndDelete(req.params.id)
+            res.json(result)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+}
